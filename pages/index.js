b6d7a0137765ac82e9f1cf12bf2803d83f164f8c
Jorge Enrique Gamboa Fuentes <snake2209@hotmail.com>
2023-01-02 00:33:18 -0500

    Unsuccessful try of making beautiful Automatic NFT Trait Generator
    Unsuccessful try of making beautiful Automatic NFT Trait Generator
    
    Tried using Buildspace template

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,41 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import metaverseProfessionalLogo from '../assets/metaverse_professional_logo.png';
+
 
 const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Automatic NFT trait generator</title>
+        <script src="https://connect.manifoldxyz.dev/latest/connect.umd.js"></script>
+        <link rel="stylesheet" href="https://connect.manifoldxyz.dev/latest/connect.css"></link>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Automatic NFT trait generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Instructions: Connect your wallet (⬇️) and select the asset that you want to get traits of</h2>
           </div>
+          
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://metaverseprofessional.tech/"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>Buildspace N&W2</p>
+          </div>
+          <div className="badge">
+            <Image src={metaverseProfessionalLogo} alt="buildspace logo" />
+            <p>Metaverse Professional</p>
           </div>
         </a>
       </div>
