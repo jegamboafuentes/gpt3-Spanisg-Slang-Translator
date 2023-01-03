214c01402612d47c186968bece1c4b599c7c65d6
Jorge Enrique Gamboa Fuentes <snake2209@hotmail.com>
2023-01-03 00:44:07 -0500

    Slang Translator generated
    It has base prompt now, and actually translate slang

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,41 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+import React from "react"
+import ReactCountryFlag from "react-country-flag"
+
+
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    //console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -11,12 +44,67 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Traductor de frases en Español entre paises Hispanos</h1>
+          </div>
+          <div className="header-title">
+            <h2>Translator of phrases in Spanish between Hispanic countries</h2>
+          </div>
+          <div className="header-title">
+            <h2>
+              <ReactCountryFlag countryCode="ES" svg />
+              <ReactCountryFlag countryCode="MX" svg />
+              <ReactCountryFlag countryCode="AR" svg />
+              <ReactCountryFlag countryCode="CL" svg />
+              <ReactCountryFlag countryCode="PE" svg />
+              <ReactCountryFlag countryCode="BO" svg />
+              <ReactCountryFlag countryCode="EC" svg />
+              <ReactCountryFlag countryCode="VE" svg />
+              <ReactCountryFlag countryCode="CO" svg />
+            </h2>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Introduce una frase de algun pais que habla Español a otro pais que tambien habla Español</h2>
+          </div>
+          <div className="header-subtitle">
+            <h3>Enter a phrase from a country that speaks Spanish to another country that also speaks Spanish</h3>
+          </div>
+          <div className="header-subtitle">
+            <h4>Ejemplo, Frase en Mexico: "Que tranza carnal"</h4>
           </div>
         </div>
+
+        <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {/* New code I added here */}
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+
       </div>
       <div className="badge-container grow">
         <a
